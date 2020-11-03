d8281952a41768eb2f618bdef3fdabd6ab010f26
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-03 19:01:46 +0530

    Add files via upload
    A       A 
     B     B  
      C   C   
       D D    
        E

---
+++ 03 Nov 20/8 Pattern 660.py
@@ -0,0 +1,12 @@
+n = 5
+a = 1
+b = n * 2 - 1
+for i in range(1, n + 1):
+    for j in range(1, n * 2 + 1):
+        if j == a or j == b:
+            print(chr(i+64), end="")
+        else:
+            print(" ", end="")
+    a += 1
+    b -= 1
+    print()
