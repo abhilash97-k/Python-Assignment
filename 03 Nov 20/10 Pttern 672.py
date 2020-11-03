feec3d0e4f1cf006d8870f28df55d23239275ce9
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-03 19:11:25 +0530

    Add files via upload
        *     
       * *    
      *   *   
     *     *  
    *       * 
     *     *
      *   *
       * *
        *

---
+++ 03 Nov 20/10 Pttern 672.py
@@ -0,0 +1,19 @@
+a = b = n = 5
+for i in range(1, n+1):
+    for j in range(1, n * 2 + 1):
+        if j == a or j == b:
+            print('*', end="")
+        else:
+            print(" ", end="")
+    a -= 1
+    b += 1
+    print()
+for i in range(n-1,0, -1):
+    for j in range(1,n-i+1):
+        print(" ", end="")
+    for j in range(1, 2*i):
+        if j==1 or j==2*i-1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
