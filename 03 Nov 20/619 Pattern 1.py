4f8f740d1a7e0ccefbbabe2b0cfe59137eb8982d
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-03 16:45:55 +0530

    Add files via upload
        1     
       2 2    
      3   3   
     4     4  
    5       5

---
+++ 03 Nov 20/619 Pattern 1.py
@@ -0,0 +1,10 @@
+a = b = n = 5
+for i in range(1, n+1):
+    for j in range(1, n * 2 + 1):
+        if j == a or j == b:
+            print(i, end="")
+        else:
+            print(" ", end="")
+    a -= 1
+    b += 1
+    print()
