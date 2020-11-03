9c1e1498f382cacc3d2cb55c3a1cbf6fdaf9a7de
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-03 18:06:13 +0530

    Add files via upload
        E     
       D D    
      C   C   
     B     B  
    A       A

---
+++ 03 Nov 20/4 Pattern 637.py
@@ -0,0 +1,11 @@
+a = b = n = 5
+for i in range(1, n+1):
+    for j in range(1, n * 2 + 1):
+        if j == a or j == b:
+            print(chr(a+64), end="")
+        else:
+            print(" ", end="")
+    a -= 1
+    b += 1
+    print()
+
