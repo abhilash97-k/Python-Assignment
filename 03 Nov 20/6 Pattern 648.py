54cea45781dc3543fb0dad1611ed971548c44aaf
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-03 18:51:03 +0530

    Add files via upload
    1       1 
     2     2  
      3   3   
       4 4    
        5

---
+++ 03 Nov 20/6 Pattern 648.py
@@ -0,0 +1,12 @@
+n = 5
+a = 1
+b = n * 2 - 1
+for i in range(1, n + 1):
+    for j in range(1, n * 2 + 1):
+        if j == a or j == b:
+            print(i, end="")
+        else:
+            print(" ", end="")
+    a += 1
+    b -= 1
+    print()
