3e22bcc427d4ed4cbcc339fb7242619bbd54a888
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-06 11:02:19 +0530

    Division of two numbers

---
+++ 10 Oct 20/04Division.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the number"))
+b = int(input("Enter second number"))
+c  = a / b
+print("Division of two numbers in intiger", int (c))
+print("Division 1of two numbers in float", c)
