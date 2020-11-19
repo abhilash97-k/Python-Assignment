b35228b65acc75e04d3782891895d7bb34231681
abhilash97-k <68045643+abhilash97-k@users.noreply.github.com>
2020-11-19 21:19:00 +0530

    Addition and Subtraction with Decor

---
+++ 19 Nov 20/01DecorAdditionAndSub.py
@@ -0,0 +1,18 @@
+#def add(a, b):
+   # print(a+b)
+
+def decor(c):
+    def mul(a, b):
+        if a % 2 == 0 and b % 2 != 0:
+            print(a * b)
+        else:
+            c(a, b)
+    return mul
+
+@decor
+def add(a, b):
+    print(a/b)
+
+add(2, 3)
+add(3, 3)
+
